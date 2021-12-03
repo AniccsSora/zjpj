d564708baf8c58431a4313f5f431dc9556b38330
aniccasora_yzu <aniccasora@yzu.edu.tw@yzu.edu.tw>
2021-12-03 20:58:40 +0800

    棄用 data aug

--- main.py
+++ main.py
@@ -1,97 +1,46 @@
-# -*- coding: utf-8 -*-
-import argparse
-from misc.voc_xml_parser import VocParser
-from misc import logger
 import torch
-import imgaug
-import imageio
-import matplotlib.pyplot as plt
+from dataloader.PatchesDataset import PatchesDataset
+from torch.utils.data import DataLoader
+from model import QRCode_CNN
+import train
+import time
+from datetime import datetime
+from os.path import join as pjoin
 import os
-import misc.F as F
-from misc.F import ensure_folder
-from imgaug.augmentables.bbs import BoundingBoxesOnImage
-from imgaug.augmentables.bbs import BoundingBox
-import imgaug.augmenters as iaa
-from imageEnhance import ImEnhance
-import misc.logger as loggerr
-from random import randint, uniform
-from tqdm import tqdm
-from misc.myparser import YoloLabelReader
-from glob import glob
-
-logger = loggerr.Logger(level=logger.logging_INFO)
-
-parser = argparse.ArgumentParser()
-
-parser.add_argument('--label_path', type=str, default=r'./data/label-qr-code', help='標記檔的資料夾')
-parser.add_argument('--img_path', type=str, default=r'./data/raw_qr', help='原始QRCODE的資料夾')
-parser.add_argument('--number', type=int, default=3, help="將一張圖強化幾次")
-parser.add_argument('--channel-check', action='store_true', help="當 img_path 內的圖片有可能出現alpha通道時，會先處理該folder內的所有圖片。")
-parser.add_argument('--verbose', action='store_true', help="印出一堆訊息")
-parser.add_argument('--show_augment', action='store_true', help="強化的結果印在圖片上")
-args = parser.parse_args()
-
-show_augment_dir_name = "show_augment"
-
-# HARDCODE
-args.show_augment = True
-
-if args.channel_check:
-    F.img_folder_chk(dir=args.img_path, logger=logger)
-
-if args.show_augment:
-    ensure_folder(os.path.join(logger.get_log_dir(), show_augment_dir_name))
-
-logger.info("================= args =================")
-for k, v in args.__dict__.items():
-    logger.info('{}: {}'.format(k, v))
-logger.info("========================================")
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    test0 = True
-    test1 = False
-    test2 = False
 
-    if test0:
-        label_reader = YoloLabelReader(label_dir=args.label_path, image_dir=args.img_path)
-        fn_list = F.get_image_filenames(args.img_path, full_path=False)
-        enhancer = ImEnhance()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-        pbar = tqdm(fn_list)  #  ['qr_0005']
-        for fn in pbar:  # ['qr_0009', 'qr_0010']:  # fn_list
-            for idx in range(args.number):  # 單張圖片的強化數量
-                label_x = label_reader[fn]
-                (image_aug, bbs_aug), params = enhancer.augument(label_x)
-                rename_fn = fn+f'_{idx}'
-                pbar.set_description("處理圖片 {} r={}".format(rename_fn, params['rotation']))
-                im_path = os.path.join(logger.get_log_dir(), f'{rename_fn}.jpg')
-                if args.show_augment:
-                    image_after_aug = bbs_aug.draw_on_image(image_aug, size=5, color=[255, 0, 0])
-                    rd = params['rotation']  # 取得選轉角度
-                    img_aug_path = os.path.join(logger.get_log_dir(), show_augment_dir_name, f'{fn}_r_{rd}.jpg')
-                    imageio.imsave(img_aug_path, im=image_after_aug)
-                F.write_label_and_image2(im_path, image_aug, rename_fn, bbs_aug, logger, allow_negative=False)
-            logger.info("{} argument complete!".format(rename_fn))
+    patches_dataset = PatchesDataset(qrcode_patches_dir='./data/pathes_of_qrcode_32x32',
+                                     background_patches_dir='./data/background_patch',
+                                     device=device)
+    val_dataset = PatchesDataset(qrcode_patches_dir='./data/val_patch_True',
+                                 background_patches_dir='./data/val_patch_False',
+                                 device=device)
 
-        F.dataset_split(logger.get_log_dir(), _logger=logger)
+    data_weight = patches_dataset.weight
+    patch_dataloader = DataLoader(patches_dataset, batch_size=64, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=512, shuffle=True)
 
-        # for yolov5 traning
-        F.command_gen(aug_number=args.number, epochs=300,
-                      name='aug',
-                      data="train_my_qr.yaml",
-                      cfg="yolov5s.yaml",
-                      batch_size=96)
+    model_dir = "model_weight"
+    net = QRCode_CNN(drop=0.1)  # drop: droplayer
 
-    if test1:
-        # labeling data
-        annotation_data = VocParser(args.label_path).get_dlist()
+    starttime = time.time()
+    current_time = datetime.now().strftime('%Y%m%d_%H%M')
+    save_path = pjoin(model_dir, current_time)
+    os.makedirs(save_path)
+    net, loss = train.train(patch_dataloader, net=net, lr=1e-4, epochs=50, weight=data_weight,
+                            draw=save_path,
+                            val_dataloader=val_dataloader)
 
-        enhancer = ImEnhance()
+    loss_train = loss['train']
+    loss_val = [_.item() for _ in loss['val']]
 
-        print(f"logging dir: {logger.get_log_dir()}. (success exit!)")
+    torch.save(net.state_dict(), f'./model_weight/{current_time}.pt')
 
-        F.dataset_split(logger.get_log_dir())
-
-    if test2:
-        F.dataset_split(logger.get_log_dir())
+    plt.plot(loss_train, label='train')
+    plt.plot(loss_val, label='val')
+    plt.legend()
