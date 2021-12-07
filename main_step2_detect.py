eba657b3f20ae5bcfd72c45a357092c573d798e8
aniccasora_yzu <aniccasora@yzu.edu.tw@yzu.edu.tw>
2021-12-07 21:54:40 +0800

    add multi scaling function

--- main_step2_detect.py
+++ main_step2_detect.py
@@ -3,8 +3,25 @@ import torch
 import torch.nn as nn
 from dataloader.PatchesDataset import get_Dataloader
 from torch.nn.functional import one_hot
+import numpy as np
+import cv2
+import argparse
+
+parser = argparse.ArgumentParser("main2 參數設定")
+# parser.add_argument("--")
+param = parser.parse_args()
+
+def scalling_img(img: np.ndarray, scale_list=[0.3, 0.5, 0.7]):
+    res_list = []
+    w, h = img.shape[1::-1]
+    for scale in scale_list:
+        re_w, re_h = int(w*scale), int(h*scale)
+        res_list.append(cv2.resize(img, dsize=(re_w, re_h), interpolation=cv2.INTER_AREA))
+    return res_list
+
 
 if __name__ == "__main__":
+    pred_img = cv2.imread("./data/paper_qr/File 088.bmp", cv2.IMREAD_GRAYSCALE)
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     weight_path = "./log_save/20211206_2247_31/weight.pt"
     qr_dir = "./data/val_patch_True"
@@ -13,16 +30,22 @@ if __name__ == "__main__":
     net = model.QRCode_CNN()
     net.load_state_dict(torch.load(weight_path))
     net.cuda()
-    with torch.no_grad():
-        loss_criterion = nn.CrossEntropyLoss()
-        total_loss, cnt = 0, 0
-        for data in val_dataloader:
-            images, labels = data
-            # 因為是練好的 model 預期出來需要是機率
-            # 並且要跟 one-hot 算 loss，(分布與分布之間的 loss)
-            pred_y = net(images).softmax(dim=1)
-            labels = one_hot(labels.long(), num_classes=2)
-            total_loss += loss_criterion(labels.float(), pred_y)
-            cnt += 1
-        avg_loss = total_loss/cnt
-        print("val_loss:", avg_loss.detach().cpu().numpy())
+
+    resize_imgs = scalling_img(pred_img)
+
+
+
+    # =============== 計算 loss
+    # with torch.no_grad():
+    #     loss_criterion = nn.CrossEntropyLoss()
+    #     total_loss, cnt = 0, 0
+    #     for data in val_dataloader:
+    #         images, labels = data
+    #         # 因為是練好的 model 預期出來需要是機率
+    #         # 並且要跟 one-hot 算 loss，(分布與分布之間的 loss)
+    #         pred_y = net(images).softmax(dim=1)
+    #         labels = one_hot(labels.long(), num_classes=2)
+    #         total_loss += loss_criterion(labels.float(), pred_y)
+    #         cnt += 1
+    #     avg_loss = total_loss/cnt
+    #     print("val_loss:", avg_loss.detach().cpu().numpy())
