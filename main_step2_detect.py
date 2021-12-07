e520206308b00ca98874e8db90821dce008ab386
aniccasora_yzu <aniccasora@yzu.edu.tw@yzu.edu.tw>
2021-12-07 21:40:35 +0800

    可以正確計算 val_loss

--- main_step2_detect.py
+++ main_step2_detect.py
@@ -1,6 +1,8 @@
 import model
 import torch
+import torch.nn as nn
 from dataloader.PatchesDataset import get_Dataloader
+from torch.nn.functional import one_hot
 
 if __name__ == "__main__":
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -10,8 +12,17 @@ if __name__ == "__main__":
     val_dataloader = get_Dataloader(qrcode_dir=qr_dir, background_dir=bg_dir)
     net = model.QRCode_CNN()
     net.load_state_dict(torch.load(weight_path))
-    net.eval()
     net.cuda()
-    for data in val_dataloader:
-        images, labels = data
-        print(net(images))
+    with torch.no_grad():
+        loss_criterion = nn.CrossEntropyLoss()
+        total_loss, cnt = 0, 0
+        for data in val_dataloader:
+            images, labels = data
+            # 因為是練好的 model 預期出來需要是機率
+            # 並且要跟 one-hot 算 loss，(分布與分布之間的 loss)
+            pred_y = net(images).softmax(dim=1)
+            labels = one_hot(labels.long(), num_classes=2)
+            total_loss += loss_criterion(labels.float(), pred_y)
+            cnt += 1
+        avg_loss = total_loss/cnt
+        print("val_loss:", avg_loss.detach().cpu().numpy())
