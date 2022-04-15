0e4e3a68758695faee705e6e633b8e6cc2dea38b
anicca <aniccasora@gmail.com>
2022-04-16 00:46:03 +0800

    自動標記新datasetOKOK

--- fullauto_labeling_patches.py
+++ fullauto_labeling_patches.py
@@ -7,9 +7,10 @@ import os
 from os.path import join as pjoin
 import random
 import numpy as np
+import matplotlib.pyplot as plt
+import tqdm
 
 predefined_class = ['qr-code', "bad-qr-code"]
-
 
 def plot_one_box(x, image, color=None, label=None, line_thickness=None):
     """
@@ -38,7 +39,7 @@ def plot_one_box(x, image, color=None, label=None, line_thickness=None):
     return image
 
 #  函數：在一幅圖片對應位置上加上矩形框  image_name 圖片名稱不含後綴
-def draw_box_on_image(image, colors, c_and_bboxes):
+def draw_box_on_image(image, colors, c_and_bboxes, shut_up=False):
 
     width, height = image.shape[1::-1]
 
@@ -48,12 +49,13 @@ def draw_box_on_image(image, colors, c_and_bboxes):
         box_class = predefined_class[int(c_b[0])]  # 拿名稱
         x, y, w, h = c_b[1:]  # 拿 box 參數
         x, y, w, h = float(x), float(y), float(w), float(h)
-        print(x, y, w, h, box_class)
-        print("\t xywh:("
-              f"{round((x - w / 2) * width)},"
-              f"{round((y - h / 2) * height)},"
-              f"{round(w*width)},"
-              f"{round(h*height)})")
+        if not shut_up:
+            print(x, y, w, h, box_class)
+            print("\t xywh:("
+                  f"{round((x - w / 2) * width)},"
+                  f"{round((y - h / 2) * height)},"
+                  f"{round(w*width)},"
+                  f"{round(h*height)})")
 
         x1 = round((x - w / 2) * width)
         y1 = round((y - h / 2) * height)
@@ -67,7 +69,7 @@ def draw_box_on_image(image, colors, c_and_bboxes):
     return box_number, image
 
 
-def get_32x32_boxes(img, c_and_bboxes, overlap=0.5):
+def get_32x32_boxes(img, c_and_bboxes, overlap=0.5, shut_up=False):
     """
 
     @param img: ndarray
@@ -102,10 +104,39 @@ def get_32x32_boxes(img, c_and_bboxes, overlap=0.5):
             _y += jump
             _x = x1
 
-        print(f"\t 共有 {sb} 個子框\n")
+        if not shut_up:
+            print(f"\t 共有 {sb} 個子框\n")
 
     return boxes_32x32
 
+def yoloxywh2xyxy(yoloxywh, w, h):
+    yolo_x, yolo_y, yolo_w, yolo_h = yoloxywh
+
+    x1 = (w*yolo_x) - (w*yolo_w/2)
+    y1 = (h*yolo_y) - (h*yolo_h/2)
+    x2 = x1 + w * yolo_w
+    y2 = y1 + h * yolo_h
+    assert x1 >= 0
+    assert y1 >= 0
+    assert x2 >= 0
+    assert y2 >= 0
+    return int(x1), int(y1), int(x2), int(y2)
+
+def resize_in_limit(image, limit):
+    """
+    @param image: ndarray 圖片
+    @param limit: 最大邊長限制
+    @return: 一個最長邊只有 limit 的 ndarray
+    """
+    w, h = image.shape[1::-1]
+    if w > h:
+        ratio = limit / w
+    else:
+        ratio = limit / h
+
+    image = cv2.resize(image, (int(w*ratio), int(h*ratio)))
+
+    return image
 
 DIR_S = {
     "label_dir": "./data_clean/the_real593_label",  # 圖片集的 yolo format.txt 根目錄
@@ -119,8 +150,8 @@ if __name__ == "__main__":
     # 使用此程式前需要準備下二資料夾，內部已經存放好資料:
     # 1. QRCodes 資料夾 (多張圖片)
     # 2. 提供另外一個資料夾路徑，其內部只包含對應名稱的 label 資訊 (使用 yolo format)
-    """ 將 QRCode 切成 32 x 32，這邊特別處理 bbox 內部的 QRCode 
-        因為當 QRCode 傾斜 45度時會造成 四邊的 patch 框的不屬於 QRCode """
+    """ 全自動無腦 patch """
+    resize_list = [1.0, 1.2, 1.3, 1.5, 1.8, 2.0]  # 縮放用
     # annotations_dir: 都是 txt(yolo註記法)
     # img_dir:圖片，與annotations_dir註記檔案名稱同名ㄟㄟㄟㄟㄟ。
     qr_code_dataset = QRCodeDataset(annotations_dir=DIR_S['label_dir'],
@@ -137,22 +168,30 @@ if __name__ == "__main__":
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # range() 內第一個參數可控制從第幾張圖片開始。
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    for data_idx in range(34, len(qr_code_dataset)):
+    pbar = tqdm.tqdm(range(0, len(qr_code_dataset)), smoothing=0.1, ncols=100)
+    for data_idx in pbar:
         data = qr_code_dataset[data_idx]
         c_and_bboxes = data[0]  # class and bounding box
         image = data[1]  # 圖片本身
         image_name = data[2]
+        pbar.set_description("processing: \"{}\"".format(image_name))
+        MAX_SIDE_LIMIT = 666
+        # 將最長邊限定在 MAX_SIDE_LIMIT
+        image = resize_in_limit(image, limit=MAX_SIDE_LIMIT)
 
         sub_savedir = pjoin(qr_code_patches_save_dir, image_name)
         os.makedirs(sub_savedir)
 
-        box_number, lined_image = draw_box_on_image(np.array(image), colors=(0, 0, 255), c_and_bboxes=c_and_bboxes)
+        box_number, lined_image = draw_box_on_image(np.array(image), colors=(0, 0, 255),
+                                                    c_and_bboxes=c_and_bboxes,
+                                                    shut_up=True)
 
         # cv2.imshow("origin", image)
         if __VISUAL_DEMO__:
             cv2.imshow(f"[Demo] box_number = {box_number}", lined_image)
 
-        boxes_32x32 = get_32x32_boxes(image, c_and_bboxes, overlap=1)
+        boxes_32x32 = get_32x32_boxes(image, c_and_bboxes, overlap=1,
+                                      shut_up=True)
 
         lb_image = np.array(image)
         if image.ndim == 2:
@@ -175,71 +214,34 @@ if __name__ == "__main__":
         # 默認此次 bbox 框的都是 full qrcode (滿滿的 qrcode)
         jump_FLAG = False
 
-        # bbox的放大率，預設為 1.0
-        bbox_zoom = 1.0
 
-        SHOW_WNAME = "output"
+
         for idx, mini_box in enumerate(boxes_32x32):
-            if bbox_zoom == 1.0:
-                lft, rbm = mini_box[0:2], mini_box[2:]  # left top, right bottom
-            else:
-                lft, rbm = mini_box[0:2], mini_box[2:]
-                rbm = (int(mini_box[2:][0]+(32*bbox_zoom-32)), int(mini_box[2:][1]+(32*bbox_zoom-32)))
-            cv2.rectangle(_tmp, lft, rbm, (0, 255, 0), 1, cv2.LINE_AA)
-            if not jump_FLAG:
-                cv2.namedWindow(SHOW_WNAME, cv2.WINDOW_NORMAL)
-                #cv2.imshow(f"({idx+1}/{len(boxes_32x32)}) O or X or R", _tmp)
-                print(f"image=\"{image_name}\"")
-                imS = cv2.resize(_tmp, (900, 900))  # Resize image
-                cv2.imshow(SHOW_WNAME, imS)
-                print("A: 全擷取, O:框框為patch, X:pass, R:設定 ratio(需要回終端輸入)")
-                print(lft, rbm)
-
-            while True:
-                if not jump_FLAG:
-                    k = cv2.waitKey(0) & 0xFF
-                if (k == ord('o') or k == ord('O')) or jump_FLAG:
-                    print(f"{idx+1} 按下了 o")
-                    if bbox_zoom == 1.0:
-                        simg = image[lft[1]:rbm[1], lft[0]:rbm[0]]
-                    else:
-                        base_size = 32
-                        _add_range = int(base_size * bbox_zoom - base_size)
-                        assert _add_range > 0
-                        simg = image[lft[1]:rbm[1]+_add_range, lft[0]:rbm[0]+_add_range]
-                    sname = pjoin(sub_savedir, f"{save_name_cnt}.bmp")
-                    cv2.imwrite(sname, simg)
-                    cv2.destroyAllWindows()
-                    save_name_cnt += 1
-                    break
-                elif k == ord('x') or k == ord('X'):
-                    print(f"{idx+1} 按下了 x")
-                    cv2.destroyAllWindows()
-                    break
-                # 跳過這這張圖片，並默認全部都是 qrcode
-                elif k == ord('a') or k == ord('A'):
-                    print(f"{idx + 1} 按下了 A")
-                    print("默認全部均是 qrcode patch")
-                    jump_FLAG = True
-                elif k == ord('r') or k == ord('R'):
-                    print(f"{idx + 1} 按下了 r")
-                    print("重新採樣 這張的 resolution")
-                    new_bbox_zoom = float(input("框框要變大幾倍?:"))
-                    print(f"bbox 放大率從 {bbox_zoom} --> {new_bbox_zoom}")
-                    bbox_zoom = new_bbox_zoom
-                    if bbox_zoom != 1.0:
-                        _postfix = "{:.1f}".format(bbox_zoom).replace('.', '_')
-                        # 把舊的先刪掉
-                        #os.rmdir(sub_savedir)
-                        sub_savedir = f"{sub_savedir}_r_{_postfix}"
-                        while os.path.exists(sub_savedir):
-                            sub_savedir += '+'
-                        os.makedirs(sub_savedir)
+            for resizeIdx, bbox_zoom in enumerate(resize_list):
+                if bbox_zoom == 1.0:
+                    lft, rbm = mini_box[0:2], mini_box[2:]  # left top, right bottom
                 else:
-                    continue
+                    lft, rbm = mini_box[0:2], mini_box[2:]
+                    rbm = (int(mini_box[2:][0]+(32*bbox_zoom-32)), int(mini_box[2:][1]+(32*bbox_zoom-32)))
 
-            # reset 畫布
-            _tmp = np.array(cnv)
+                if bbox_zoom == 1.0:
+                    simg = image[lft[1]:rbm[1], lft[0]:rbm[0]]
+                else:
+                    base_size = 32
+                    _add_range = int(base_size * bbox_zoom - base_size)
+                    assert _add_range > 0
+                    simg = image[lft[1]:rbm[1]+_add_range, lft[0]:rbm[0]+_add_range]
+                # 名稱
+                # sname = pjoin(sub_savedir, f"{save_name_cnt}.bmp")
+                sname = pjoin(sub_savedir, f"{resizeIdx}_{str(idx).zfill(3)}.bmp")
+                try:
+                    cv2.imwrite(sname, simg)
+                except Exception as e:
+                    pass
+                #save_name_cnt += 1
+                # reset 畫布
+                _tmp = np.array(cnv)
+            # End of resize
         # end of one image.
 
 
